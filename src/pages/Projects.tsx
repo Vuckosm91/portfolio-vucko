005d901557340f634b15bc65288b9d5afc5b109d
Vucko <vuckosm91@gmail.com>
2024-06-10 02:30:36 +0200

    added project page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,61 @@
-import { ProjectPageWrapp } from "./ProjectsStyled";
+import { LinkToWeb, TextStyledM } from "./HomeStyled";
+import image from "../images/games-platform.png";
+
+import {
+  GamePlatformPic,
+  GamePlatformPicText,
+  GamePlatformPicWrapper,
+  GamePlatformText,
+  ListItem,
+  ProjectPageWrapp,
+  StyledList,
+} from "./ProjectsStyled";
 
 const Projects = () => {
-  return <ProjectPageWrapp></ProjectPageWrapp>;
+  return (
+    <ProjectPageWrapp>
+      <GamePlatformText>
+        <TextStyledM>Project Description</TextStyledM>
+        <TextStyledM>
+          This project is a comprehensive gaming platform where users can
+          explore a wide variety of games fetched from a backend server. The
+          platform provides robust filtering options allowing users to sort
+          games by genre, platform, release year, title, and relevance.
+          Additionally, users can search for specific game titles. Each game
+          listed on the platform comes with detailed information including an
+          average rating, supported platforms, and publisher details. When a
+          user selects a game, they are taken to a dedicated page for that game,
+          featuring a detailed description, screenshots, and a trailer (if
+          available from the backend). This platform aims to enhance the gaming
+          discovery experience by providing intuitive and user-friendly
+          navigation and search capabilities, ensuring that users can find and
+          learn about games that match their interests.
+          <br />
+          <br />
+          <LinkToWeb
+            href="https://game-hub-ruby-iota.vercel.app/"
+            target="blank"
+          >
+            Try demo here
+          </LinkToWeb>
+        </TextStyledM>
+      </GamePlatformText>
+      <GamePlatformPicWrapper>
+        <img src={image} />
+        <GamePlatformPicText>
+          <TextStyledM>What was used making this app:</TextStyledM>
+          <StyledList>
+            <ListItem>TSX (TypeScript XML)</ListItem>
+            <ListItem>CSS</ListItem>
+            <ListItem>Chakra UI</ListItem>
+            <ListItem>ReactQuery</ListItem>
+            <ListItem>Zustand (State managment)</ListItem>
+            <ListItem>Vercel (deployment)</ListItem>
+          </StyledList>
+        </GamePlatformPicText>
+      </GamePlatformPicWrapper>
+    </ProjectPageWrapp>
+  );
 };
 
 export default Projects;
--- src/pages/ProjectsStyled.tsx
+++ src/pages/ProjectsStyled.tsx
@@ -1,8 +1,66 @@
 import styled from "styled-components";
+import image from "../images/games-platform.png";
 
 export const ProjectPageWrapp = styled.div`
   display: flex;
+  justify-content: space-around;
   height: auto;
   width: 100%;
-  border: 1px solid red;
+  @media (max-width: 1080px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const GamePlatformText = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  height: auto;
+  width: 50%;
+  @media (max-width: 1080px) {
+    width: 90%;
+  }
+`;
+
+export const GamePlatformPicWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  height: auto;
+  width: 40%;
+  @media (max-width: 1080px) {
+    width: 90%;
+  }
+`;
+
+export const GamePlatformPicText = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+  height: auto;
+  width: 100%;
+`;
+
+export const GamePlatformPic = styled.div`
+  display: flex;
+  width: 100%;
+  height: 600px;
+  border: 1px solid green;
+  background: url(${image});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: start;
+`;
+
+export const StyledList = styled.ul`
+  list-style-type: disc;
+  padding-left: 1.5rem;
+  color: #add8e6;
+  font-family: "Arial", sans-serif;
+  font-size: 1.2rem;
+`;
+
+export const ListItem = styled.li`
+  margin-bottom: 0.5rem; /* Razmak između stavki u listi */
 `;
